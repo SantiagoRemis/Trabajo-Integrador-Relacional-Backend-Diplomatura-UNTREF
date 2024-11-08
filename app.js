8a07e30023416a4c67e2949242f21c8c1dc68bb3
SantiagoRemis <santiremis@gmail.com>
2024-11-07 23:19:31 -0300

    conexion base de datos

--- app.js
+++ app.js
@@ -1,15 +1,67 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const contenidoRoutes = require('./routes/contenidoRoutes');
-const db = require('./conexion/database');
+const contenidoRoutes = require("./routes/contenidoRoutes");
+const sequelize = require("./conexion/database.js");
 
 // Middlewares
 app.use(express.json());
-app.use('/contenido', contenidoRoutes);
+
+// // Requiere los modelos
+// const Contenido = require("./models/contenido");
+// const Categoria = require("./models/categoria");
+// const Genero = require("./models/genero");
+// const Actor = require("./models/actor");
+// const ContenidoGenero = require("./models/contenido_genero");
+// const ContenidoActor = require("./models/contenido_actor");
+
+// // Configura las relaciones entre los modelos
+// Contenido.belongsTo(Categoria, { foreignKey: "id_categoria" });
+// Categoria.hasMany(Contenido, { foreignKey: "id_categoria" });
+
+// // Relación muchos a muchos con Actor
+// Contenido.belongsToMany(Actor, {
+//   through: "contenido_actor",
+//   foreignKey: "id_contenido",
+//   otherKey: "id_actor",
+//   as: "contenidoActores", // Alias único para la relación con actores
+// });
+
+// Actor.belongsToMany(Contenido, {
+//   through: "contenido_actor",
+//   foreignKey: "id_actor",
+//   otherKey: "id_contenido",
+//   as: "actorContenidos", // Alias único para la relación con actores
+// });
+
+// // Sincronización de la base de datos
+// db.sequelize
+//   .sync({ force: false }) // Evita borrar tablas si ya existen
+//   .then(() => {
+//     console.log("Base de datos sincronizada correctamente");
+//   })
+//   .catch((error) => {
+//     console.error("Error al sincronizar la base de datos:", error);
+//   });
+
+app.use(async (req, res, next) => {
+  try {
+    await sequelize.authenticate();
+    console.log("Conexión establecida con exito ! =)");
+    next();
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Error en el servidor: ", description: error.message });
+  }
+});
+
+app.get("/hola", (req, res) => {
+  res.send("¡Hola Mundo!");
+});
+// app.use("/contenido", contenidoRoutes);
 
 // Server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-    
--- conexion/database.js
+++ conexion/database.js
@@ -1,18 +1,24 @@
-const mysql = require('mysql2');
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'trailerflix'
+const { Sequelize } = require("sequelize");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+// module.exports.sequelize = new Sequelize(
+//   process.env.DB_NAME, // Cambiado a DB_NAME para coincidir con .env
+//   process.env.DB_USER, // Cambiado a DB_USER
+//   process.env.DB_PASSWORD, // Cambiado a DB_PASSWORD
+//   {
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: process.env.DB_DIALECT,
+//   }
+// );
+
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
+  dialect: "mysql",
 });
 
-connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to the database:', err);
-        return;
-    }
-    console.log('Connected to MySQL database');
-});
-
-module.exports = connection;
-    
+module.exports = sequelize;
